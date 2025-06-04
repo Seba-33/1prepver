552378dd255f84fc17512564b3c89be9b1ac111c
Seba-33 a <mihali.sebastian@iisgalvanimi.edu.it>
2025-06-04 19:04:17 +0000

    punto 4

--- prep/src/app/app.component.ts
+++ prep/src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dati } from './Models/dati.models';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'prep';
   vettDati : Dati[] = []
+  loading!: boolean
+  obs! : Observable<object>
+  data! : object
+  http : HttpClient
+
+  constructor(http : HttpClient){this.http = http}
+
+  makeChiamata(){
+    this.loading = true
+    this.obs = this.http.get('https://my-json-server.typicode.com/malizia-g/verificaPrenotazioni/prenotazioni')
+    this.obs.subscribe(this.getData)
+  } 
+
+  getData = (d : object)=>{
+    this.data = d
+    this.loading = false
+    console.log(this.data)
+  }
+
+  ngOnInit(): void {
+    this.makeChiamata()
+  }
+
+
+
 }
