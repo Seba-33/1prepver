d1f5660edf4cc3acbcdaf7162977d06c7043e7a8
Seba-33 a <mihali.sebastian@iisgalvanimi.edu.it>
2025-06-04 18:55:16 +0000

    punto 2

---
+++ prep/src/app/Models/data.models.ts
@@ -0,0 +1,19 @@
+export class Dati {
+    data : string
+    ora : string
+    nome : string
+    cognome : string
+    indirizzo : string
+    telefono : string
+    email : string
+
+    constructor(data : string, ora : string, nome : string, cognome : string, indirizzo : string, telefono : string, email : string){
+        this.data = data
+        this.ora = ora
+        this.nome = nome
+        this.cognome = cognome
+        this.indirizzo = indirizzo
+        this.telefono = telefono
+        this.email = email
+    }
+}
